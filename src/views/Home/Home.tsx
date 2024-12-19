7411f4d918f484b0b83854ab6a6793b4023c5df3
Ramiro Quiroga <ramiro.quiroga@cepsi.net.ar>
2024-12-19 15:44:10 -0300

    agregando react native navigation stgacl

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header/Header";
 import { Button, ButtonGroup, Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
+const {navigate}=useNavigation<StackNavigationProp<RootStackParamList,'AddFood'>>()
+    const handleAddCalories=()=>{
+        navigate('AddFood')
+    }
   return (
     <View style={style.container}>
       <Header />
@@ -12,7 +17,7 @@ const Home = () => {
           <Text style={style.text}>Calorias</Text>
         </View>
         <View style={style.rigthContainer}>
-          <Button icon={<Icon name="add-cicle-outline"/>}/>
+          <Button icon={<Icon name="add-circle-outline"/>}  style={style.button} onPress={handleAddCalories}/>
         </View>
       </View>
     </View>
@@ -22,12 +27,25 @@ const Home = () => {
 const style = StyleSheet.create({
   container: {
     flex: 1,
+    gap:10,
     backgroundColor: "#FFF",
     paddingHorizontal: 20,
   },
-  articleContainer: {},
-  leftContainer: {},
-  rigthContainer: {},
+  button:{
+    borderRadius:20,
+    color:"green"
+  },
+  articleContainer: {
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  leftContainer: {
+    flex:1,
+
+  },
+  rigthContainer: {
+    
+  },
 });
 
 export default Home;
